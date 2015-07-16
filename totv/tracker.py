2da1d0f7b24f1cd3aa4d1d51a5a638491e44a3ff
toor-totv <toor@titansof.tv>
2015-07-16 15:20:04 -0600

    Added timeout for api client.

--- totv/tracker.py
+++ totv/tracker.py
@@ -101,7 +101,7 @@ class Client(object):
     """
 
     def __init__(self, api_uri, username="dev", password="dev", redis_host="localhost",
-                 redis_port=6379, redis_db=0, verify=False):
+                 redis_port=6379, redis_db=0, verify=False, timeout=3):
         self._api_uri = api_uri
         self._auth = (username, password) if username and password else None
         self._redis_host = redis_host
@@ -109,17 +109,18 @@ class Client(object):
         self._redis_db = redis_db
         self._redis = redis.StrictRedis(host=redis_host, port=int(redis_port), db=int(redis_db))
         self._verify = verify
+        self._timeout = timeout
 
     def _request(self, path, method='get', payload=None, valid_codes=None):
         if valid_codes is None:
             valid_codes = []
         if method == "get":
-            resp = requests.get(self._make_url(path), verify=self._verify, auth=self._auth)
+            resp = requests.get(self._make_url(path), verify=self._verify, auth=self._auth, timeout=self._timeout)
         elif method == "post":
             resp = requests.post(self._make_url(path), json=payload, verify=self._verify,
-                                 auth=self._auth)
+                                 auth=self._auth, timeout=self._timeout)
         elif method == "delete":
-            resp = requests.delete(self._make_url(path), verify=self._verify, auth=self._auth)
+            resp = requests.delete(self._make_url(path), verify=self._verify, auth=self._auth, timeout=self._timeout)
         else:
             raise NotImplementedError("Unsupported HTTP method: {}".format(method))
         if resp.status_code == httplib.NOT_FOUND:
